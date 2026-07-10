8118bc899f39dd8cd4988fd176de49fa4aa542bd
Subham Dey <sarmisthaad@gmail.com>
2026-07-10 21:02:33 +0530

    Contact Schema Compleated

--- src/models/contact.js
+++ src/models/contact.js
@@ -4,7 +4,26 @@ const {Schema}=mongoose;
 const contactSchema=new Schema({
     contacts:{
         required:true,
-        enum:["Father","Mother","Brother","Sister","Local Police Station","Other Contacts"],
+        enum:["Family","Father","Mother","Brother","Sister","Local Police Station","Other Contacts"],
+        default:"Family",
         type:String
-    }
+    },profileId: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },via:{
+        required:true,
+        enum:["WhatsApp","SMS"],
+        default:['SMS']
+    }, createdAt: {
+    type: Date,
+    default: Date.now
+  },contactNumber:{
+    type:String,
+    trim:true,
+    required:true
+  }
 })
+
+const Contact=mongoose.model("contact",contactSchema);
+module.export = Contact;
