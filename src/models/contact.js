268366bf7a57de49f7c15e040a3e6bd81adf967b
Subham Dey <sarmisthaad@gmail.com>
2026-07-10 21:05:06 +0530

    Contact Schema Fully Complete

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,29 +1,45 @@
-const mongoose=require('mongoose');
-const {Schema}=mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const contactSchema=new Schema({
-    contacts:{
-        required:true,
-        enum:["Family","Father","Mother","Brother","Sister","Local Police Station","Other Contacts"],
-        default:"Family",
-        type:String
-    },profileId: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },via:{
-        required:true,
-        enum:["WhatsApp","SMS"],
-        default:['SMS']
-    }, createdAt: {
+const contactSchema = new Schema({
+  contacts: {
+    required: true,
+    enum: [
+      "Family",
+      "Father",
+      "Mother",
+      "Brother",
+      "Sister",
+      "Local Police Station",
+      "Other Contacts",
+    ],
+    default: "Family",
+    type: String,
+  },
+  profileId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  via: {
+    required: true,
+    enum: ["WhatsApp", "SMS"],
+    default: ["SMS"],
+  },
+  createdAt: {
     type: Date,
-    default: Date.now
-  },contactNumber:{
-    type:String,
-    trim:true,
-    required:true
+    default: Date.now,
+  },
+  contactNumber: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  priority:{
+    type:Number,
+    default:1
   }
-})
+});
 
-const Contact=mongoose.model("contact",contactSchema);
+const Contact = mongoose.model("contact", contactSchema);
 module.export = Contact;
